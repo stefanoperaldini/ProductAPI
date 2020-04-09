4f8a41501b544369a5cb5471e5fa287c10cb6136
stefanoperaldini <stefano.peraldini@gmail.com>
2020-04-09 11:57:55 +0200

    first commit

---
+++ ProductsAPI/Model/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ProductsAPI.Model
+{
+    public class Product
+    {
+        public String ID { get; set; }
+        public String Name { get; set; }
+        public String Brand { get; set; }
+    }
+}
---
+++ ProductsAPI/Services/IProductsService.cs
@@ -0,0 +1,14 @@
+﻿using ProductsAPI.Model;
+using System.Collections.Generic;
+
+
+namespace ProductsAPI.Services
+{
+    public interface IProductsService
+    {
+        public List<Product> GetProducts();
+        public Product AddProduct(Product productItem);
+        public Product UpdateProduct(string id, Product productItem);
+        public string DeleteProduct(string id);
+    }
+}
